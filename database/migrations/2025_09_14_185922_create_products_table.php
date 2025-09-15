b90637374493e978aad94f1eaa5586ae02af781e
Kasparas-G55 <kasparas.galdikas55@gmail.com>
2025-09-15 17:24:33 +0300

    fix: product description data type set to `TEXT`

--- database/migrations/2025_09_14_185922_create_products_table.php
+++ database/migrations/2025_09_14_185922_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id('product_id');
             $table->string('sku')->unique();
-            $table->string('description');
+            $table->text('description');
             $table->string('size');
             $table->string('photo');
             $table->jsonb('tags');
